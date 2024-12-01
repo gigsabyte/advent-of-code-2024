279a3bfe718a164bb3b368181e7d1ee83b8dccf1
Gigi Bachtel <35552168+gigsabyte@users.noreply.github.com>
2024-11-30 22:20:16 -0800

    Project init
    - Basic console program, input parsing
    - Abstract parent class Solution to handle result formatting

---
+++ AOC2024/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!int.TryParse(args[0], out var day) || day < 0 || day > 25)
+            {
+                Console.Error.Write($"'{args[0]}' is not a valid day. Enter a number 1-25");
+                return;
+            }
+            
+            var solutionType = Type.GetType($"AOC2024.Solutions.Solution{day}");
+            if(solutionType == null)
+            {
+                Console.Error.Write($"Solution for day {day} is unavailable. Come back later!");
+                return;
+            }
+
+            var solution = (Solution)Activator.CreateInstance(solutionType);
+            solution.Run();
+        }
+    }
+}
---
+++ AOC2024/Solution.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AOC2024
+{
+    abstract class Solution
+    {
+        protected abstract int Day { get; }
+        protected abstract string Name { get; }
+
+        public void Run()
+        {
+            PrintTitle(out var footer);
+
+            var input = GetInput();
+
+            if(string.IsNullOrEmpty(input))
+            {
+                Console.Error.WriteLine("Could not retrieve input text. Exiting...");
+            }
+
+            Console.WriteLine($" Part 1 Result:\n {RunPart1(input)}");
+            Console.WriteLine(" --------------");
+            Console.WriteLine($" Part 2 Result:\n {RunPart2(input)}");
+            Console.WriteLine(footer);
+        }
+
+        private void PrintTitle(out string footer)
+        {
+            var title = $"Day {Day}: '{Name}'";
+
+            var decorationBuilder = new StringBuilder(title.Length + 5);
+            decorationBuilder.Append("*-");
+            for(var i = 0; i < title.Length / 3 - 1; ++i)
+            {
+                decorationBuilder.Append("*--");
+            }
+            if(title.Length % 3 > 0)
+            {
+                decorationBuilder.Append("*--");
+            }
+            decorationBuilder.Append("*-*");
+            Console.WriteLine(decorationBuilder.ToString());
+            Console.WriteLine($" {title}");
+
+            decorationBuilder.Clear();
+
+            decorationBuilder.Append("*-");
+            for (var i = 0; i < title.Length - 2; ++i)
+            {
+                decorationBuilder.Append("-");
+            }
+            decorationBuilder.Append("-*");
+            footer = decorationBuilder.ToString();
+            Console.WriteLine(footer);
+        }
+
+        private string GetInput()
+        {
+            var input = string.Empty;
+            var filepath = $"./Solutions/{Day.ToString("00")}/input.txt";
+
+            try
+            {
+                input = File.ReadAllText(filepath);
+            }
+            catch(FileNotFoundException fnfe)
+            {
+                Console.Error.WriteLine($"File at path '{filepath}' does not exist!\nMake sure to add input.txt before running solution!!!");
+                Console.Error.WriteLine($"Full error text:\n{fnfe.Message}");
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine($"Encountered an unknown error attempting to get input at file '{filepath}'.");
+                Console.Error.WriteLine($"Full error text:\n{e.Message}");
+            }
+
+            return input;
+        }
+
+        protected abstract string RunPart1(string input);
+        protected abstract string RunPart2(string input);
+    }
+}
